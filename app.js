cab627e4eab30c3f182a900feed3d42e7897bfe2
Brian McClain <brianmmcclain@gmail.com>
2019-02-26 12:45:28 -0500

    Update output to show user agent as well

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 module.exports = message => {
     const { headers, payload } = message;
-    return `Message received with Content-Type '${headers.getValue('Content-Type')}' and payload '${payload}'.`;
+    return `Content-Type: '${headers.getValue('Content-Type')}', User-Agent: '${headers.getValue('User-Agent')}' and payload '${payload}'.`;
 }
 
 // set argument type to message, default is payload
