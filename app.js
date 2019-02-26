5231b360a2dff5d47dc8f3cd5349b419f06f02ad
Brian McClain <brianmmcclain@gmail.com>
2019-02-26 12:39:54 -0500

    Integrate example from node-message

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-module.exports = x => "Hello, " + x + "!";
+module.exports = message => {
+    const { headers, payload } = message;
+    return `Message received with Content-Type '${headers.getValue('Content-Type')}' and payload '${payload}'.`;
+}
+
+// set argument type to message, default is payload
+module.exports.$argumentType = 'message';
